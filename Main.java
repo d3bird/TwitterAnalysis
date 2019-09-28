e7063d181db7deb3394bb1bf866e7e1a786f33b8
Scott Crawford <dogbird9@gmail.com>
2019-09-28 11:50:07 -0400

    Now places the tweets into data storage

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package twitter;
 
 import java.io.IOException;
+import java.util.Vector;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -19,21 +20,53 @@ public class Main {
 		Tweet example = new Tweet();
 		Tweet example2 = new Tweet("message",true, 2);//message, does it have pictures, how many
 		
+		Vector<Tweet> tweets = new Vector<Tweet>();
+		
+		//retrive the tweets
 		try {
+			
+			boolean pics = false;
+			String msg ="";
+			int nPics = -1;
+			Tweet temp;
 			Document doc = Jsoup.connect(baseline + hashtagword).get();
 			System.out.println(doc.title());
 
 			Elements paragraphs = doc.getElementsByTag("p");
 			for (Element paragraph : paragraphs) {
 				if (paragraph.text().contains(" ")) {
-					System.out.println(paragraph.text());
-					// System.out.println("");
-					System.out.println("");
-					System.out.println("");
+					
+					if(paragraph.text().contains("e've detected that JavaScript")) {
+						System.out.println("removed javascript message");
+					}else {
+					
+						 pics = false;
+						 nPics = -1;
+						 msg = paragraph.text();
+						 
+						 if (msg.contains("pic.twitter.com/")) {
+							 pics = true;
+							 nPics = 1;
+						 }
+						 
+						 temp = new Tweet(msg,pics,nPics);
+						 //System.out.println(temp.getMsg());
+						 tweets.add(temp);
+					}
+					//System.out.println("");
+					//System.out.println("");
 				}
 			}
 
-			System.out.println("");
+			System.out.println("printing out from data storage");
+			
+			for (int i =0 ; i < tweets.size();i++) {
+				System.out.println(tweets.get(i).getMsg());
+				System.out.println("");
+				System.out.println("");
+			}
+			
+			
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
